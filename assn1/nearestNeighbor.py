7ac07a6e2d139246218aa79ce47050b26957b7a6
Jose Paredes Hernandez <jpare006@ucr.edu>
2018-04-30 16:46:02 -0700

    Complete divide and conquer

--- assn1/nearestNeighbor.py
+++ assn1/nearestNeighbor.py
@@ -4,6 +4,7 @@ import sys
 import time
 import profile
 from math import sqrt
+import time
 
 # Command line arguments
 parser=argparse.ArgumentParser(description='Calculate the nearest two points on a plan')
@@ -14,17 +15,79 @@ parser.add_argument('--profile',action='store_true')
 parser.add_argument('filename',metavar='<filename>',\
     help='Input dataset of points')
 
+
+
+
+#=================================================================================================
+def bruteForce(points):
+    minimum_distance = 0;
+    distance = 0;
+    point1 = (-1,-1)
+    point2 = (-1,-1)
+    for num in range(0, len(points)):
+        for nestedNum in range(num + 1, len(points)):
+            tmp_point1 = (points[num][0], points[num][1])
+            tmp_point2 = (points[nestedNum][0], points[nestedNum][1])
+
+            distance = sqrt(((tmp_point1[0] - tmp_point2[0])**2) + ((tmp_point1[1] - tmp_point2[1])**2))
+            if num == 0 and nestedNum == 1: # this means that it is the first distance calculated
+                minimum_distance = distance
+            else:
+                if distance < minimum_distance:
+                    minimum_distance = distance
+                    point1 = (tmp_point1[0], tmp_point1[1])
+                    point2 = (tmp_point2[0], tmp_point2[1])
+    return (minimum_distance,point1,point2)
+#end def brute_force_nearest_neighbor(points):
+#================================================================================================
+
 #Divide and conquer version of the nearest neighbor algorithm
 #Input: points := unsorted array of (x,y) coordinates
 #Output: tuple of smallest distance and coordinates (distance,(x1,y1),(x2,y2))
 def divideAndConquerNearestNeighbor(points):
-    minimum_distance = 0;
-    point1 = (-1,-1)
-    point2 = (-1,-1)
+    #minimum_distance = 0;
+    #point1 = (-1,-1)
+    #point2 = (-1,-1)
     #TODO: Complete this function
-    print("Divide and Conquer algorithm is incomplete")
-    return (minimum_distance,point1,point2)
+
+    points.sort() #sorts by x-coordinate
+    if len(points) <= 3:
+        return bruteForce(points)
+    middle = int(len(points)/2)
+    midpoint = points[middle][0]
+
+    L = points[:middle]
+    R = points[middle:]
+
+    minimum_distanceL = divideAndConquerNearestNeighbor(L)
+    minimum_distanceR = divideAndConquerNearestNeighbor(R)
+
+    if minimum_distanceL[0] <= minimum_distanceR[0]:
+        minimum_distance = minimum_distanceL
+        #print("The my min dist is: " + str(minimum_distanceL[0]))
+        #return minimum_distanceL
+    else:
+        minimum_distance = minimum_distanceR
+        #print("The my min dist is: " + str(minimum_distanceR[0]))
+        #return minimum_distanceR
+
+    for point in points:
+        if point[0] < midpoint - minimum_distance[0] or point[0] > midpoint + minimum_distance[0]:
+            del(point)
+    borderMinDist = bruteForce(points)
+    
+    #print("Divide and Conquer algorithm is incomplete")
+    if minimum_distance[0] <= borderMinDist[0]:
+        #print("The following is the minimum distance: " + str(minimum_distance[0]))
+        return minimum_distance
+    else:   
+        #print("The following is the minimum distance: " + str(borderMinDist[0]))
+        return borderMinDist
 #end def divide_and_conquer(points):
+#=================================================================================================
+
+
+
 
 #=================================================================================================
 #Brute force version of the nearest neighbor algorithm
@@ -40,6 +103,10 @@ def bruteForceNearestNeighbor(points):
 
     for num in range(0, len(points)):
         for nestedNum in range(num + 1, len(points)):
+            #code used to keep track of progress (made algorithm take significantly longer)
+            #sys.stdout.write('\033[F')
+            #sys.stdout.write('\033[K') 
+            #print(str(int(num/len(points) * 100)) + "%")            
 
             #find distance between point at index num and nestedNum
             #first set up points to perform distance calculation
@@ -64,7 +131,7 @@ def bruteForceNearestNeighbor(points):
             #print(point1)
             #print(nestedNum, end = " ")
         #print("\n")
-    print("The following is the minimum distance: " + str(minimum_distance))
+    #print("The following is the minimum distance: " + str(minimum_distance))
 
     print("Brute force algorithm is incomplete")
     return (minimum_distance,point1,point2)
@@ -96,15 +163,26 @@ def main(filename,algorithm):
     result = bruteForceResult = divideAndConquerResult = None
     if algorithm == 'a' or algorithm == 'b':
         #TODO: Insert timing code here
+        t0 = time.time()
         bruteForceResult = bruteForceNearestNeighbor(points)
+        t1 = time.time()
+
+        print("Time elapsed(brute force): " + str(t1-t0))        
+
     if algorithm == 'a' or algorithm == 'd':
         #TODO: Insert timing code here
+        
+        t0 = time.time()
         divideAndConquerResult = divideAndConquerNearestNeighbor(points)
+        t1 = time.time()
+        print("Time elapsed(divide and conquer): " + str(t1-t0))
+        #print("The following is the minimum distance: " + str(divideAndConquerResult[0]))
+     
     if algorithm == 'a': # Print whether the results are equal (check)
-        if args.verbose:
-            print('Brute force result: '+str(bruteForceResult))
-            print('Divide and conquer result: '+str(divideAndConquerResult))
-            print('Algorithms produce the same result? '+str(bruteForceResult == divideAndConquerResult))
+        #if args.verbose:
+        print('Brute force result: '+str(bruteForceResult))
+        print('Divide and conquer result: '+str(divideAndConquerResult))
+        print('Algorithms produce the same result? '+str(bruteForceResult == divideAndConquerResult))
         result = bruteForceResult if bruteForceResult == divideAndConquerResult else ('Error','N/A','N/A')
     else:  
         result = bruteForceResult if bruteForceResult is not None else divideAndConquerResult
